ae0dac514df1475db595b8f685728aa38397a37f
b-lamer <152201467+b-lamer@users.noreply.github.com>
2025-02-11 14:16:08 -0600

    Update iptables.sh

--- Ubuntu18/Scripts/iptables.sh
+++ Ubuntu18/Scripts/iptables.sh
@@ -8,7 +8,11 @@ iptables -t filter -P FORWARD DROP
 
 #Splunk
 iptables -A INPUT -p tcp --dport 8089 -j ACCEPT
-iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
