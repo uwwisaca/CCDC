ebc15d92f722ec762ba3c8b533dc9aa8d842e856
McCulloch336 <McCullocTJ21@uww.edu>
2026-02-13 15:58:00 -0600

    Update sysinternalsDownload.ps1

--- Windows/sysinternalsDownload.ps1
+++ Windows/sysinternalsDownload.ps1
@@ -1,10 +1,11 @@
 $DestinationFolder = "C:\Sysinternals"
 New-Item -Path $DestinationFolder -ItemType Directory -Force
-$links = (Invoke-WebRequest -Uri 'https://live.sysinternals.com").Links
+$links = (Invoke-WebRequest -Uri 'https://live.sysinternals.com').Links
 foreach ($item in $links) {
 	if ($item.href -match '\.exe') {
 		$fileName = $item.href.Split('/')[-1]
 		Write-Host "Downloading: $fileName"
 		Invoke-WebRequest -Uri ("https://live.sysinternals.com" + $item.href) -Outfile "$DestinationFolder\$fileName" -ErrorAction Silently Continue
 	}
+
 }
